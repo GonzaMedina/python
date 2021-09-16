e13446bb595153c83cfec5c6a1dce0ee0b1341cb
Gonza Medina <81445102+elgonzaexe@users.noreply.github.com>
2021-09-16 20:07:50 -0300

    Delete template-string.js

--- introduccion-js/template-string.js
+++
@@ -1,7 +0,0 @@
-const nombre = 'Marcelo';
-const trabajo = 'Desarrollador Full web';
-
-//Concatenar las constantes
-// console.log('Nombre:'+ ' ' + ' ' + nombre + 'Trabaja:' + ' ' + trabajo);
-// Template String
-console.log(`Nombre: ${nombre}, Trabaja como: ${trabajo}`)
